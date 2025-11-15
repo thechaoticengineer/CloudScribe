717714fd62357f7fed476557543e5abd340bf4e1
thechaoticengineer <I@thechaotic.engineer>
2025-11-15 22:16:38 +0100

    Refactor Notes API endpoints: enhance response consistency, add status codes, and improve formatting

--- src/services/CloudScribe.Notes.API/Api/Endpoints/NotesEndpoints.cs
+++ src/services/CloudScribe.Notes.API/Api/Endpoints/NotesEndpoints.cs
@@ -3,6 +3,7 @@ using CloudScribe.Notes.API.Domain;
 using CloudScribe.Notes.API.Services;
 using Microsoft.AspNetCore.Http.HttpResults;
 using CloudScribe.SharedKernel;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CloudScribe.Notes.API.Api.Endpoints;
 
@@ -15,48 +16,52 @@ public static class NotesEndpoints
             .WithOpenApi();
 
         group.MapGet("", async (NotesService service, int pageNumber = 1, int pageSize = 10) =>
-            Results.Ok(await service.GetAll(pageNumber, pageSize)))
+                Results.Ok(await service.GetAll(pageNumber, pageSize)))
             .Produces<PagedResult<Note>>()
             .WithDescription("Get all notes ordered by created date descending");
 
         group.MapGet("{id:guid}", async Task<Results<Ok<Note>, NotFound>> (Guid id, NotesService service) =>
-        {
-            var note = await service.GetByIdAsync(id);
-            return note is not null
-                ? TypedResults.Ok(note)
-                : TypedResults.NotFound();
-        })
-        .Produces<Note>()
-        .WithDescription("Get a note by id");
+            {
+                var note = await service.GetByIdAsync(id);
+                return note is not null
+                    ? TypedResults.Ok(note)
+                    : TypedResults.NotFound();
+            })
+            .Produces<Note>()
+            .Produces(StatusCodes.Status404NotFound)
+            .WithDescription("Get a note by id");
 
         group.MapPost("", async (CreateNoteRequest request, NotesService service) =>
-        {
-            var note = await service.Create(request.Title, request.Content);
-            return Results.Created($"/api/notes/{note.Id}", note);
-        })
-        .Produces<Note>(StatusCodes.Status201Created) 
-        .WithDescription("Create a new note");
+            {
+                //Todo: add validation
+                var note = await service.Create(request.Title, request.Content);
+                return Results.Created($"/api/notes/{note.Id}", note);
+            })
+            .Produces<Note>(StatusCodes.Status201Created)
+            .WithDescription("Create a new note");
 
-        group.MapPut("{id:guid}", async Task<Results<Ok<Note>, NotFound>> (Guid id, UpdateNoteRequest request, NotesService service) =>
-        {
-            var note = await service.Update(id, request.Title, request.Content);
-            return note is not null
-                ? TypedResults.Ok(note)
-                : TypedResults.NotFound();
-        }).Produces<Note>()
-        .Produces<Note>()
-        .Produces(StatusCodes.Status404NotFound)
-        .WithDescription("Update an existing note");
+        group.MapPut("{id:guid}",
+                async Task<Results<Ok<Note>, NotFound>> (Guid id, UpdateNoteRequest request, NotesService service) =>
+                {
+                    //Todo: add validation
+                    var note = await service.Update(id, request.Title, request.Content);
+                    return note is not null
+                        ? TypedResults.Ok(note)
+                        : TypedResults.NotFound();
+                })
+            .Produces<Note>()
+            .Produces(StatusCodes.Status404NotFound)
+            .WithDescription("Update an existing note");
 
         group.MapDelete("{id:guid}", async Task<Results<NoContent, NotFound>> (Guid id, NotesService service) =>
-        {
-            var deleted = await service.Delete(id);
-            return deleted
-                ? TypedResults.NoContent()
-                : TypedResults.NotFound();
-        }).Produces(StatusCodes.Status204NoContent)
-        .Produces(StatusCodes.Status404NotFound)
-        .WithDescription("Delete an existing note");
+            {
+                var deleted = await service.Delete(id);
+                return deleted
+                    ? TypedResults.NoContent()
+                    : TypedResults.NotFound();
+            }).Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithDescription("Delete an existing note");
 
         return app;
     }
