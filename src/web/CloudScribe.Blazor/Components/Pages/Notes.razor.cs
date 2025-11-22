40108ce1748317d7e6001b1106873e2aef07ab96
thechaoticengineer <I@thechaotic.engineer>
2025-11-22 15:51:33 +0100

    Add form for creating notes and display submitted note list

--- src/web/CloudScribe.Blazor/Components/Pages/Notes.razor.cs
+++ src/web/CloudScribe.Blazor/Components/Pages/Notes.razor.cs
@@ -1,7 +1,54 @@
+using System.ComponentModel.DataAnnotations;
+using CloudScribe.Contracts.Notes;
+using CloudScribe.SharedKernel;
 using Microsoft.AspNetCore.Components;
 
 namespace CloudScribe.Blazor.Components.Pages;
 
-public partial class Notes : ComponentBase
+public partial class Notes(HttpClient http) : ComponentBase
 {
+    private CreateNote Request { get; set; } = new();
+    private PagedResult<NoteDto>? _notes;
+
+    private async Task OnSubmit()
+    {
+        try
+        {
+            await http.PostAsJsonAsync("/api/notes", new CreateNoteRequest(Request.Title, Request.Content));
+            _notes = await http.GetFromJsonAsync<PagedResult<NoteDto>>("/api/notes?pageNumber=1&pageSize=10");
+        }
+        catch (Exception e)
+        {
+        }
+        
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            _notes = await http.GetFromJsonAsync<PagedResult<NoteDto>>("/api/notes?pageNumber=1&pageSize=10");
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+}
+
+public class CreateNote
+{
+    [Required(ErrorMessage = "Title is required.")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Content is required.")]
+    public string Content { get; set; } = string.Empty;
+}
+
+public class NoteDto
+{
+    public Guid Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string Content { get; init; } = string.Empty;
+    public DateTime CreatedUtc { get; init;  }
+    public DateTime ModifiedUtc { get; init; }
 }
